8922fc5698951804867c0866ba2561f4a9b9fb64
Eduard <goby145csgo@gmail.com>
2022-08-06 12:29:15 +0200

    improve suspens

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { Layout } from "antd";
 import React, { Suspense } from "react";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 
-const Main = React.lazy(() => import("./components/main/Main"));
+const Content = React.lazy(() => import("./components/content/Content"));
 
 function App() {
   return (
     <div>
       <Suspense fallback={<p>Loading...</p>}>
-        <Main />
+        <Layout style={{ minHeight: "100vh" }}>
+          <Navbar />
+          <Content />
+          <Footer />
+        </Layout>
       </Suspense>
     </div>
   );
