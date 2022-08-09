88004e1fa24f907920bf34f863a1c4929d7daa66
muradImM <muradsrage@gmail.com>
2022-08-09 08:21:16 +0300

    feat(routes): added routes directory for public and private routes, utils directory for const values; modified routes implementation in Router.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,20 @@
 import { useTranslation } from "react-i18next";
 import NavLink from "../UI/navlink/NavLink";
 import NavbarStyles from "./style";
+import {
+  DASHBOARD_PAGE_ROUTE,
+  HOME_PAGE_ROUTE,
+  REDUX_TEST_PAGE_ROUTE,
+} from "../../utils/consts";
 
 function Navbar() {
   const { t } = useTranslation();
 
   return (
     <NavbarStyles>
-      <NavLink path="/">{t("Home.title")}</NavLink>
-      <NavLink path="/dashboard">{t("Dashboard.title")}</NavLink>
-      <NavLink path="/redux-test">{t("Redux.title")}</NavLink>
+      <NavLink path={HOME_PAGE_ROUTE}>{t("Home.title")}</NavLink>
+      <NavLink path={DASHBOARD_PAGE_ROUTE}>{t("Dashboard.title")}</NavLink>
+      <NavLink path={REDUX_TEST_PAGE_ROUTE}>{t("Redux.title")}</NavLink>
     </NavbarStyles>
   );
 }
---
+++ src/utils/consts.ts
@@ -0,0 +1,3 @@
+export const HOME_PAGE_ROUTE = "/";
+export const DASHBOARD_PAGE_ROUTE = "/dashboard";
+export const REDUX_TEST_PAGE_ROUTE = "/redux-test";
