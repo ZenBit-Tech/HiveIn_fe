e417f558baa31c0e22a33c42e89396d689c48232
muradImM <muradsrage@gmail.com>
2022-08-17 15:35:04 +0300

    fix(settings menu links): fixed styles for settings menu links

--- src/components/settingsMenu/SettingsMenu.tsx
+++ src/components/settingsMenu/SettingsMenu.tsx
@@ -3,14 +3,25 @@ import { NavLink } from "react-router-dom";
 import i18next from "localization/en/en.json";
 import { SETTINGS_CONTACT_INFO_ROUTE, SETTINGS_ROUTE } from "utils/routeConsts";
 import { SButton } from "components/settingsMenu/styles";
+import { PRIMARY, DISABLED } from "../../utils/colorConsts";
 
 function SettingsMenu() {
   return (
     <div>
-      <NavLink to={SETTINGS_ROUTE}>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: PRIMARY } : { color: DISABLED }
+        }
+        to={SETTINGS_ROUTE}
+      >
         <SButton>{i18next.settingMenuButtonsNames.profile}</SButton>
       </NavLink>
-      <NavLink to={SETTINGS_CONTACT_INFO_ROUTE}>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: PRIMARY } : { color: DISABLED }
+        }
+        to={SETTINGS_CONTACT_INFO_ROUTE}
+      >
         <SButton>{i18next.settingMenuButtonsNames.contacts}</SButton>
       </NavLink>
     </div>
--- src/components/settingsMenu/styles.ts
+++ src/components/settingsMenu/styles.ts
@@ -1,18 +1,14 @@
 import styled from "styled-components";
-import { DISABLED, PRIMARY } from "utils/colorConsts";
+import { PRIMARY } from "utils/colorConsts";
 
 /* eslint-disable import/prefer-default-export */
 export const SButton = styled("button")`
   display: block;
-  color: ${DISABLED};
   background-color: inherit;
   border: 1px solid ${PRIMARY};
   width: 120px;
   padding: 5px;
   margin-bottom: 5px;
   border-radius: 5px;
-
-  &:focus {
-    color: ${PRIMARY};
-  }
+  color: inherit;
 `;
