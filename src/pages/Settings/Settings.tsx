2a2f8f87fc32af459c25f859dd2e1943c2e2f714
Eduard <goby145csgo@gmail.com>
2022-08-17 19:18:24 +0200

    integrate a component & improve template

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Row } from "antd";
 import SettingsMenu from "components/settingsMenu/SettingsMenu";
 import ProfileEditForm from "components/profileEditForm/ProfileEditForm";
+import PhotoUpload from "components/photoUpload/PhotoUpload";
 
 function Settings() {
   return (
@@ -9,8 +10,11 @@ function Settings() {
       <Col span={4}>
         <SettingsMenu />
       </Col>
-      <Col span={20}>
+      <Col span={16}>
         <ProfileEditForm />
+      </Col>
+      <Col span={4}>
+        <PhotoUpload />
       </Col>
     </Row>
   );
