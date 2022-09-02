14c5032d37066a482adec2c1b01c889ac84b087b
Joao Hinning <jphinning@gmail.com>
2022-09-01 23:36:13 -0300

    fix: Remove unnecessary comment

--- src/components/UI/navBarDrawer/MenuDrawer.tsx
+++ src/components/UI/navBarDrawer/MenuDrawer.tsx
@@ -50,24 +50,6 @@ function MenuDrawer({ drawerLinks }: MenuDrawerProps) {
               <Divider />
             </Row>
           ))}
-          {/* <Row>
-          <Col span={12}>
-            <NavLink path="/search-work">{t("SearchWork.title")}</NavLink>
-          </Col>
-          <Divider />
-        </Row>
-        <Row>
-          <Col span={12}>
-            <NavLink path="/proposals">{t("Proposals.title")}</NavLink>
-          </Col>
-          <Divider />
-        </Row>
-        <Row>
-          <Col span={12}>
-            <NavLink path="/my-contracts">{t("MyContracts.title")}</NavLink>
-          </Col>
-          <Divider />
-        </Row> */}
           <Row>
             <Col span={12}>
               <NavLink path="/login" onClick={signOut}>
