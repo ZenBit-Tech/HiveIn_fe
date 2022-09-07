9c851b98d17d2fe4c981eeb8ac0a256d35ab0385
caiquedegaspari <caique.degaspare@gmail.com>
2022-09-06 21:05:48 -0300

    change user interface

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,33 +5,34 @@ import logo from "components/navbar/imgs/logo.svg";
 import useViewport from "hooks/useViewport";
 import NavBarButton from "components/UI/navBarButton/NavBarButton";
 import MenuDrawer from "components/UI/navBarDrawer/MenuDrawer";
-import {
-  SettingFilled,
-  UserOutlined,
-  MessageFilled,
-  LogoutOutlined,
-} from "@ant-design/icons";
+import { LogoutOutlined, UserOutlined } from "@ant-design/icons";
 import {
   SEARCH_WORK_ROUTE,
   HOME_PAGE_ROUTE,
   PROPOSALS_ROUTE,
   MY_JOBS_ROUTE,
-  CHAT_ROUTE,
   SETTINGS_ROUTE,
-  PROFILE_ROUTE,
   SIGN_UP_ROUTE,
   SIGN_IN_ROUTE,
-  CLIENT_PROFILE,
   CLIENT_HOME,
   MY_CONTRACTS_ROUTE,
+  SETTINGS_CONTACT_INFO_ROUTE,
+  CLIENT_PROFILE,
+  CHAT_ROUTE,
 } from "utils/routeConsts";
 import useAuth from "hooks/useAuth";
 import { MOBILE_SCREEN_SIZE } from "utils/navBarConsts";
 import { useEffect, useState } from "react";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
 
-interface NavLinkType {
+interface LinkType {
   title: string;
   to: string;
+}
+interface NavLinkType {
+  title: string;
+  links: LinkType[];
 }
 
 function Navbar() {
@@ -44,32 +45,64 @@ function Navbar() {
     if (role === "freelancer") {
       setNavLinks([
         {
-          to: SEARCH_WORK_ROUTE,
-          title: t("SearchWork.title"),
+          title: t("MyJobs.jobs"),
+          links: [
+            {
+              to: SEARCH_WORK_ROUTE,
+              title: t("SearchWork.title"),
+            },
+            {
+              to: PROPOSALS_ROUTE,
+              title: t("Proposals.title"),
+            },
+            {
+              title: t("MyJobs.title"),
+              to: MY_JOBS_ROUTE,
+            },
+            {
+              to: MY_CONTRACTS_ROUTE,
+              title: t("MyContracts.title"),
+            },
+          ],
         },
         {
-          to: PROPOSALS_ROUTE,
-          title: t("Proposals.title"),
-        },
-        {
-          title: t("MyJobs.title"),
-          to: MY_JOBS_ROUTE,
-        },
-        {
-          to: MY_CONTRACTS_ROUTE,
-          title: t("MyContracts.title"),
+          title: t("Profile.title"),
+          links: [
+            {
+              to: SETTINGS_CONTACT_INFO_ROUTE,
+              title: t("Profile.title"),
+            },
+            {
+              to: SETTINGS_ROUTE,
+              title: t("Settings.title"),
+            },
+          ],
         },
       ]);
     }
     if (role === "client") {
       setNavLinks([
         {
-          to: CLIENT_HOME,
-          title: "Jobs",
+          title: t("MyJobs.jobs"),
+          links: [
+            {
+              to: MY_JOBS_ROUTE,
+              title: "Jobs",
+            },
+            {
+              to: CLIENT_HOME,
+              title: "Talent",
+            },
+          ],
         },
         {
-          to: CLIENT_HOME,
-          title: "Talent",
+          title: t("Profile.title"),
+          links: [
+            {
+              to: CLIENT_PROFILE,
+              title: t("Profile.title"),
+            },
+          ],
         },
       ]);
     }
@@ -93,43 +126,29 @@ function Navbar() {
       <NavLink path={role === "freelancer" ? HOME_PAGE_ROUTE : CLIENT_HOME}>
         <img height="50px" alt="logo" src={logo} />
       </NavLink>
-
-      {screenWidth < MOBILE_SCREEN_SIZE ? (
-        ""
-      ) : (
-        <>
-          {navLinks.map(({ title, to }) => (
-            <NavLink key={to} path={to}>
-              {title}
-            </NavLink>
-          ))}
-        </>
-      )}
-
+      <Menu
+        defaultSelectedKeys={["jobs"]}
+        mode="horizontal"
+        style={{ width: "100%" }}
+      >
+        {navLinks.map(({ links, title }) => (
+          <Menu.SubMenu
+            icon={title === t("Profile.title") ? <UserOutlined /> : undefined}
+            key={title}
+            title={title}
+          >
+            {links.map((link) => (
+              <Menu.Item key={link.to}>
+                <Link to={link.to}>{link.title}</Link>
+              </Menu.Item>
+            ))}
+          </Menu.SubMenu>
+        ))}
+        <Menu.Item key={t("Chat.title")}>
+          <Link to={CHAT_ROUTE}>{t("Chat.title")}</Link>
+        </Menu.Item>
+      </Menu>
       <NavBarButtons>
-        <NavLink path={CHAT_ROUTE}>
-          <NavBarButton icon={<MessageFilled />} title={t("Chat.title")} />
-        </NavLink>
-        {role === "freelancer" ? (
-          <>
-            <NavLink path={SETTINGS_ROUTE}>
-              <NavBarButton
-                icon={<SettingFilled />}
-                title={t("Settings.title")}
-              />
-            </NavLink>
-            <NavLink path={PROFILE_ROUTE}>
-              <NavBarButton
-                icon={<UserOutlined />}
-                title={t("Profile.title")}
-              />
-            </NavLink>
-          </>
-        ) : (
-          <NavLink path={CLIENT_PROFILE}>
-            <NavBarButton icon={<UserOutlined />} title={t("Profile.title")} />
-          </NavLink>
-        )}
         <NavBarButton
           icon={<LogoutOutlined />}
           title={t("SignIn.signOut")}
